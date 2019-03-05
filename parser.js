f794a726f0d74058db307d3b594e9a743dcc970a
Jose Garcia <pepe_garcia1606@yahoo.com>
2019-03-05 14:10:44 -0600

    Update parser.js

--- parser.js
+++ parser.js
@@ -10,16 +10,18 @@ module.exports = {
         mainURL = "https://www.vokal.io/blog"
         return new Promise( (resolve, reject) => {
             request( {uri: mainURL}, (error, response, body) => {
+                // If there is an error in the request then finish the async request
                 if (error) {
                     reject(error)
                 }
                 const blogTitles = []
-                const dom = new JSDOM(`${body}`)
-                let blogs = dom.window.document.querySelectorAll("h2")
+                const dom = new JSDOM(`${body}`) // Loading HTML source document
+                let blogs = dom.window.document.querySelectorAll("h2") // Reading all h2's in this case the titles of the blogs
                 blogs.forEach( (blog) => {
                     let blogTitle = blog.textContent.replace(new RegExp(String.raw`\n`,"g"), "").trim()
                     blogTitles.push(blogTitle)
                 })
+                // Returning only the number of blogs specified by the user
                 resolve(blogTitles.splice(0,numberOfBlogs))
             })
         })
@@ -27,6 +29,7 @@ module.exports = {
     },
 
     getBlogURL(blogTitle) {
+        // TODO: Description of blogs
         let newTitle = blogTitle.toLowerCase()
         let expTitle = newTitle.replace(new RegExp(" ", "g"), "-") // g = Global match; find all matches
         return `${mainURL}/${expTitle}`
